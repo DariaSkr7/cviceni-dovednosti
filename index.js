67c55eeec41b9302ebd56a9452c8d4d7053e8dfc
Daria Skoryk <dariaskoryk@gmail.com>
2023-09-29 17:50:52 +0200

    final result

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log('funguju!');
+
+const html = Number(prompt('What is your level of HTML?'));
+const css = Number(prompt('What is your level of CSS?'));
+const javaScript = Number(prompt('What is your level of JS?'));
+
+const updateSkill = (id, level) => {
+  const element = document.querySelector(`#${id} .skill__value`);
+  element.textContent = `${level} / 100`;
+  const elementProgress = document.querySelector(`#${id} .skill__progress`);
+  elementProgress.style.width = `${level}%`;
+};
+
+updateSkill('skill1', html);
+updateSkill('skill2', css);
+updateSkill('skill3', javaScript);
